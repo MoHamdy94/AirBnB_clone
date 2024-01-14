c983cccb4c25a496ba98f166a07dd813f2d6e2a7
Mohamdy <mohamedhamdy994@gmail.com>
2024-01-15 00:53:48 +0200

    commit

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,73 +1,123 @@
-#!/usr/bin/python3
-''' test Place class
-    - test instantiation
-    - test to_dict
-'''
+#!/usr/bin/python3s
+"""
+    test for the user model
+"""
 
 import unittest
-from datetime import datetime
 from models.base_model import BaseModel
-from time import sleep
 from models.place import Place
-from models.engine.file_storage import FileStorage
 
 
-class TestPlace_instantiation(unittest.TestCase):
-    '''Unittest for User class'''
+class TestUser(unittest.TestCase):
+    """
+        Testing Place class
+    """
 
-    def test_model_instantiation(self):
-        self.assertEqual(Place, type(Place()))
+    def setUp(self):
+        """
+            Creates an instance for place.
+        """
+        self.new_place = Place()
 
-    def test_att(self):
-        inst = Place()
-        self.assertEqual(str, type(inst.name))
-        self.assertEqual(str, type(inst.city_id))
-        self.assertEqual(str, type(inst.user_id))
-        self.assertEqual(str, type(inst.description))
-        self.assertEqual(int, type(inst.number_rooms))
-        self.assertEqual(int, type(inst.number_bathrooms))
-        self.assertEqual(int, type(inst.number_bathrooms))
-        self.assertEqual(int, type(inst.max_guest))
-        self.assertEqual(int, type(inst.price_by_night))
-        self.assertEqual(float, type(inst.latitude))
-        self.assertEqual(float, type(inst.longitude))
-        self.assertEqual(list, type(inst.amenity_ids))
+    def TearDown(self):
+        pass
 
-    def test_created_at(self):
-        self.assertEqual(datetime, type(Place().created_at))
+    def test_Place_inheritance(self):
+        """
+            tests that the City class Inherits from BaseModel
+        """
 
-    def test_id(self):
-        id1 = Place()
-        id2 = Place()
-        self.assertNotEqual(id1.id, id2.id)
+        self.assertIsInstance(self.new_place, BaseModel)
 
+    def test_Place_attributes(self):
+        """
+            Checks that the attribute exist.
+        """
+        self.assertTrue("city_id" in self.new_place.__dir__())
+        self.assertTrue("user_id" in self.new_place.__dir__())
+        self.assertTrue("description" in self.new_place.__dir__())
+        self.assertTrue("name" in self.new_place.__dir__())
+        self.assertTrue("number_rooms" in self.new_place.__dir__())
+        self.assertTrue("max_guest" in self.new_place.__dir__())
+        self.assertTrue("price_by_night" in self.new_place.__dir__())
+        self.assertTrue("latitude" in self.new_place.__dir__())
+        self.assertTrue("longitude" in self.new_place.__dir__())
+        self.assertTrue("amenity_ids" in self.new_place.__dir__())
 
-class TestPlace_to_dict(unittest.TestCase):
+    def test_type_amenity(self):
+        """
+            Test the type of latitude
+        """
+        amenity = getattr(self.new_place, "amenity_ids")
+        self.assertIsInstance(amenity, list)
 
-    def test_check_type(self):
-        inst = Place()
-        self.assertTrue(dict, type(inst.to_dict))
+    def test_type_longitude(self):
+        """
+            Test the type of longitude.
+        """
+        longitude = getattr(self.new_place, "longitude")
+        self.assertIsInstance(longitude, float)
 
-    def test_key_check(self):
-        inst = Place()
-        self.assertIn("id", inst.to_dict())
-        self.assertIn("created_at", inst.to_dict())
-        self.assertIn("updated_at", inst.to_dict())
-        self.assertIn("__class__", inst.to_dict())
-
-    def test_date_time(self):
-        inst = Place()
-        inst_dict = inst.to_dict()
-        self.assertTrue(str, type(inst_dict["created_at"]))
-        self.assertTrue(str, type(inst_dict["updated_at"]))
-
-    def test_save(self):
-        inst = Place()
-        sleep(0.05)
-        first_updated_at = inst.updated_at
-        inst.save()
-        self.assertLess(first_updated_at, inst.updated_at)
+    def test_type_latitude(self):
+        """
+            Test the type of latitude
+        """
+        latitude = getattr(self.new_place, "latitude")
+        self.assertIsInstance(latitude, float)
 
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_type_price_by_night(self):
+        """
+            Test the type of price_by_night
+        """
+        price_by_night = getattr(self.new_place, "price_by_night")
+        self.assertIsInstance(price_by_night, int)
+
+    def test_type_max_guest(self):
+        """
+            Test the type of max_guest
+        """
+        max_guest = getattr(self.new_place, "max_guest")
+        self.assertIsInstance(max_guest, int)
+
+    def test_type_number_bathrooms(self):
+        """
+            Test the type of number_bathrooms
+        """
+        number_bathrooms = getattr(self.new_place, "number_bathrooms")
+        self.assertIsInstance(number_bathrooms, int)
+
+    def test_type_number_rooms(self):
+        """
+            Test the type of number_bathrooms
+        """
+        number_rooms = getattr(self.new_place, "number_rooms")
+        self.assertIsInstance(number_rooms, int)
+
+    def test_type_description(self):
+        """
+            Test the type of description
+        """
+        description = getattr(self.new_place, "description")
+        self.assertIsInstance(description, str)
+
+    def test_type_name(self):
+        """
+            Test the type of name
+        """
+        name = getattr(self.new_place, "name")
+        self.assertIsInstance(name, str)
+
+    def test_type_user_id(self):
+        """
+            Test the type of user_id
+        """
+        user_id = getattr(self.new_place, "user_id")
+        self.assertIsInstance(user_id, str)
+
+    def test_type_city_id(self):
+        """
+            Test the type of city_id
+        """
+        city_id = getattr(self.new_place, "city_id")
+        self.assertIsInstance(city_id, str)
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,65 +1,45 @@
 #!/usr/bin/python3
-''' test  Review class
-    - test instantiation
-    - test to_dict
-'''
+
+"""
+    test for the user model.
+"""
 
 import unittest
-from datetime import datetime
 from models.base_model import BaseModel
-from time import sleep
 from models.review import Review
-from models.engine.file_storage import FileStorage
 
 
-class TestReview_instantiation(unittest.TestCase):
-    '''Unittest for User class'''
+class TestReview(unittest.TestCase):
+    """
+        Testing Review class
+    """
 
-    def test_model_instantiation(self):
-        self.assertEqual(Review, type(Review()))
+    def test_Review_inheritance(self):
+        """
+            tests that the Review class Inherits from BaseModel
+        """
+        new_review = Review()
+        self.assertIsInstance(new_review, BaseModel)
 
-    def test_att(self):
-        inst = Review()
-        self.assertEqual(str, type(inst.place_id))
-        self.assertEqual(str, type(inst.user_id))
-        self.assertEqual(str, type(inst.text))
-        self.assertEqual(str, type(inst.id))
+    def test_Review_attributes(self):
+        """
+            Test that Review class has place_id, user_id and text
+            attributes.
+        """
+        new_review = Review()
+        self.assertTrue("place_id" in new_review.__dir__())
+        self.assertTrue("user_id" in new_review.__dir__())
+        self.assertTrue("text" in new_review.__dir__())
 
-    def test_created_at(self):
-        self.assertEqual(datetime, type(Review().created_at))
-
-    def test_id_equal(self):
-        id1 = Review()
-        id2 = Review()
-        self.assertNotEqual(id1.id, id2.id)
-
-
-class TestReview_to_dict(unittest.TestCase):
-
-    def test_check_type(self):
-        inst = Review()
-        self.assertTrue(dict, type(inst.to_dict))
-
-    def test_key_check(self):
-        inst = Review()
-        self.assertIn("id", inst.to_dict())
-        self.assertIn("created_at", inst.to_dict())
-        self.assertIn("updated_at", inst.to_dict())
-        self.assertIn("__class__", inst.to_dict())
-
-    def test_date_time(self):
-        inst = Review()
-        inst_dict = inst.to_dict()
-        self.assertTrue(str, type(inst_dict["created_at"]))
-        self.assertTrue(str, type(inst_dict["updated_at"]))
-
-    def test_save(self):
-        inst = Review()
-        sleep(0.05)
-        first_updated_at = inst.updated_at
-        inst.save()
-        self.assertLess(first_updated_at, inst.updated_at)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_Review_attributes(self):
+        """
+            Test that Review class has place_id, user_id and text
+            attributes.
+        """
+        new_review = Review()
+        place_id = getattr(new_review, "place_id")
+        user_id = getattr(new_review, "user_id")
+        text = getattr(new_review, "text")
+        self.assertIsInstance(place_id, str)
+        self.assertIsInstance(user_id, str)
+        self.assertIsInstance(text, str)
